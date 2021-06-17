a8b50b32b43a35fa38260af49c5741629d6bf61c
Shruti Agrawal <shruti98932@gmail.com>
2021-06-17 11:22:58 -0400

    Changes made for indentation

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,23 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  //var someInstance = {}
   this.storage = {};
+  this.key =0;
 
 };
 
-var key = 0;
 
 Stack.prototype.push = function(value) {
-  key++;
-  this.storage[key] = value;
+  this.key++;
+  this.storage[this.key] = value;
 };
 
 Stack.prototype.pop = function() {
-  var del = this.storage[key];
-  delete this.storage[key];
-  key--;
-  return del;
+  var del = this.storage[this.key];
+  delete this.storage[this.key];
+  this.key--;
+    return del;
 }
 
 Stack.prototype.size = function() {
